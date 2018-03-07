6a7ff0034bf21964b32f8d22fb4bd935004f2010
FrankBustamante <frankbustab@gmail.com>
2018-03-06 19:20:56 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+  	apiKey: "AIzaSyC2-lUSPFedr1fWy1MaXZSYLJXsRtRC9y8",
+    authDomain: "login-1a69f.firebaseapp.com",
+    databaseURL: "https://login-1a69f.firebaseio.com",
+    projectId: "login-1a69f",
+    storageBucket: "",
+    messagingSenderId: "1044154609966"
+  }
 };
